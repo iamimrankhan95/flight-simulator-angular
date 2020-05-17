6fee63654445606292dd500efab9a1fcff515044
thanos <imran.khan@synesisit.com.bd>
2020-05-17 17:43:57 +0600

    empty project

---
+++ src/app/_nav.ts
@@ -0,0 +1,13 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+  },
+  {
+    title: true,
+    name: 'Components'
+  }
+];
