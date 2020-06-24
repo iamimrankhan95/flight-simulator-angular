44450a2fc47cd0f844d870a07c3d44adde50e1ac
[Alif <alif.ahmed@synesisit.com.bd>
2020-06-24 15:30:43 +0600

    commented out error handling from user data service

--- src/app/modules/user/user-data.service.ts
+++ src/app/modules/user/user-data.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { User } from '../../shared/models/user';
-import { tap, catchError } from 'rxjs/operators';
-import { HttpErrorHandler, HandleError } from '../../core/http-error-handler.service';
+// import { tap, catchError } from 'rxjs/operators';
+// import { HttpErrorHandler, HandleError } from '../../core/http-error-handler.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,52 +14,59 @@ export class UserDataService {
   };
 
   public url = 'http://192.168.101.41:9050/cms_login'; // URL to web api
-  private handleError: HandleError;
+
+  // Error Handling portions are commented out
+  // private handleError: HandleError;
 
   constructor(
     private router: Router,
     private http: HttpClient,
-    httpErrorHandler: HttpErrorHandler
+    // httpErrorHandler: HttpErrorHandler
     ) {
-      this.handleError = httpErrorHandler.createHandleError('UserDataService');
+      // this.handleError = httpErrorHandler.createHandleError('UserDataService');
     }
 
   register(user: User) {
-    return this.http.post<User>(this.url, user, this.httpOptions).pipe(
-      tap((newUser: User) => console.log(`added user w/ id=${newUser.id}`)),
-      catchError(this.handleError<User>('register', user))
-    );
+    return this.http.post<User>(this.url, user, this.httpOptions);
+    // .pipe(
+    //   tap((newUser: User) => console.log(`added user w/ id=${newUser.id}`)),
+    //   catchError(this.handleError<User>('register', user))
+    // );
   }
 
   getUsers() {
-    return this.http.get<any>(this.url).pipe(
-      tap(_ => console.log('fetched Users')),
-      catchError(this.handleError('getUsers', []))
-    );
+    return this.http.get<any>(this.url);
+    // .pipe(
+    //   tap(_ => console.log('fetched Users')),
+    //   catchError(this.handleError('getUsers', []))
+    // );
   }
 
   deleteUser(user: User) {
     const id = user.id;
     const tempUrl = this.url + '/' + id;
-    return this.http.delete<User>(tempUrl, this.httpOptions).pipe(
-      tap((_) => console.log(`deleted user id=${id}`)),
-      catchError(this.handleError<User>('deleteUser', user))
-      );
+    return this.http.delete<User>(tempUrl, this.httpOptions);
+    // .pipe(
+    //   tap((_) => console.log(`deleted user id=${id}`)),
+    //   catchError(this.handleError<User>('deleteUser', user))
+    //   );
   }
 
   getUser(userId: number) {
     const tempUrl = this.url + '/' + userId;
-    return this.http.get<User>(tempUrl).pipe(
-      tap((_) => console.log(`fetched user id=${userId}`)),
-      catchError(this.handleError<User>(`getUser id=${userId}`))
-    );
+    return this.http.get<User>(tempUrl);
+    // .pipe(
+    //   tap((_) => console.log(`fetched user id=${userId}`)),
+    //   catchError(this.handleError<User>(`getUser id=${userId}`))
+    // );
   }
 
   updateUser(user: User, id: number) {
     const tempUrl = this.url + '/' + id;
-    return this.http.put<User>(tempUrl, user, this.httpOptions).pipe(
-      tap((_) => console.log(`updated user id=${id}`)),
-      catchError(this.handleError<any>('updateUser', user))
-    );
+    return this.http.put<User>(tempUrl, user, this.httpOptions);
+    // .pipe(
+    //   tap((_) => console.log(`updated user id=${id}`)),
+    //   catchError(this.handleError<any>('updateUser', user))
+    // );
   }
 }
