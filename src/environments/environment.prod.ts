94aea156e8eff20c66eaa221d2aeaed185851579
thanos <imran.khan@synesisit.com.bd>
2020-07-01 14:22:38 +0600

    refact environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://localhost:8200/',
+  fileServiceBaseUrl: 'http://103.92.84.244:9004/',
+  apiUrl: 'http://172.16.26.167:4001/',
 };
