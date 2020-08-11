be6d624a099b794c19c364179381a6ccc47e43bf
thanos <imran.khan@synesisit.com.bd>
2020-08-11 15:34:26 +0600

    show form

--- src/app/modules/flight-simulator/flight-simulator.component.ts
+++ src/app/modules/flight-simulator/flight-simulator.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ConfirmationDialogService } from '../../shared/modules/notification/confirmation-dialog/confirmation-dialog.service';
+import { onlyNumeric, nidValidation } from '../../shared/services/validation-forms.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TicketStatus } from '../../shared/models/dto/ticket-status-dto';
+import { AppConstant } from '../../shared/enums/Constants';
 
 @Component({
   selector: 'app-flight-simulator',
@@ -7,9 +14,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlightSimulatorComponent implements OnInit {
 
-  constructor() { }
+  isCollapsed: boolean = false;
+  ticketStatuses: TicketStatus[] = AppConstant.ticketStatus;
+  today = new Date();
+  fromMinDate = { year: this.today.getFullYear() - 100, month: 1, day: 1 };
+  fromMaxDate = { year: this.today.getFullYear(), month: this.today.getMonth() + 1, day: this.today.getDate() };
+  maritalStatus = AppConstant.maritalStatus;
+  gender = AppConstant.gender;
+  isFormSubmitted = false;
+
+  crmForm = this.fb.group({
+    id: [''],
+    uniqueid: [''],
+    agentId: [''],
+    msisdn: ['', [Validators.required]],
+    contactNo: ['', [Validators.required]],
+    nidNumber: ['', [Validators.required, onlyNumeric(), nidValidation()]],
+    compliantName: ['', [Validators.required]],
+    gender: ['', [Validators.required]],
+    email: [''],
+    maritalStatus: [''],
+    isHusband: [true, [Validators.required]],
+    spouseName: [''],
+    fatherName: [''],
+    fatherOrHusbandName: ['', [Validators.required]],
+    motherName: ['', [Validators.required]],
+    dob: [''],
+    occupation: ['', [Validators.required]],
+    accusedOrganizationName: ['', [Validators.required]],
+    problemDescription: ['', [Validators.required, Validators.maxLength(500)]],
+    ticketStatus: ['2', [Validators.required]],
+    applicationType: ['1', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder,
+    private confirmationDialogService: ConfirmationDialogService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  async onSubmit() {
   }
 
 }
--- src/app/modules/flight-simulator/flight-simulator.module.ts
+++ src/app/modules/flight-simulator/flight-simulator.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { FlightSimulatorRoutingModule } from './flight-simulator-routing.module';
 import { FlightSimulatorComponent } from './flight-simulator.component';
+import { SharedModule } from '../../shared/modules/shared/shared.module';
 
 
 @NgModule({
   declarations: [FlightSimulatorComponent],
   imports: [
     CommonModule,
+    SharedModule,
     FlightSimulatorRoutingModule
   ]
 })
