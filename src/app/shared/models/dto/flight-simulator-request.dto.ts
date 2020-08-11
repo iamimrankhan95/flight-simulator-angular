d393eb1d68c9b2d8b84780696b62916fde4e8de5
thanos <imran.khan@synesisit.com.bd>
2020-08-11 15:59:45 +0600

    add models

---
+++ src/app/shared/models/dto/flight-simulator-request.dto.ts
@@ -0,0 +1,6 @@
+export interface FlightSimulatorRequest {
+  DepartureAirportCode: string; // = "MEl",
+  ArrivalAirportCode: string; // = "LHR",
+  DepartureDate: string; // = "2012-12-24T00:00:00+11:00",
+  ReturnDate: string; // = "2013-01-03T00:00:00+11:00"
+}
---
+++ src/app/shared/models/dto/flight-simulator-response.dto.ts
@@ -0,0 +1,9 @@
+export interface FlightSimulatorResponseObject {
+  AirlineLogoAddress: string; // "http://nmflightapi.azurewebsites.net/Images/AirlineLogo/CZ.gif",
+  AirlineName: string; // "China Southern Airlines",
+  InboundFlightsDuration: string; // "24:10",
+  ItineraryId: string; // "",
+  OutboundFlightsDuration: string; // "26:20",
+  Stops: number; // 2,
+  TotalAmount: number; // 2903.84
+}
