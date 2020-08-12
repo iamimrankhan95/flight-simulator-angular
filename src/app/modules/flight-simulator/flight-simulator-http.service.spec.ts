0d1e5220cdc4012f165d231c40d9b5f63fa1dfb9
thanos <imran.khan@synesisit.com.bd>
2020-08-12 12:07:39 +0600

    add test

--- src/app/modules/flight-simulator/flight-simulator-http.service.spec.ts
+++ src/app/modules/flight-simulator/flight-simulator-http.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FlightSimulatorHttpService } from './flight-simulator-http.service';
+import { FlightSimulatorRequest } from '../../shared/models/dto/flight-simulator-request.dto';
+import { FlightSimulatorResponseObject } from '../../shared/models/dto/flight-simulator-response.dto';
+import { FlightSimulatorService } from './flight-simulator.service';
 
 describe('FlightSimulatorHttpService', () => {
   let service: FlightSimulatorHttpService;
+  let serviceData: FlightSimulatorService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [FlightSimulatorHttpService, FlightSimulatorService]
+    });
     service = TestBed.inject(FlightSimulatorHttpService);
+    serviceData = TestBed.inject(FlightSimulatorService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be retrive all flights', () => {
+    let flightSimulatorRequest: FlightSimulatorRequest = {
+      DepartureAirportCode: '',
+      ArrivalAirportCode: '',
+      DepartureDate: '',
+      ReturnDate: ''
+    };
+    flightSimulatorRequest.DepartureAirportCode = 'MEl';
+    flightSimulatorRequest.ArrivalAirportCode = 'LHR';
+    flightSimulatorRequest.DepartureDate = '2012-12-24T00:00:00+11:00';
+    flightSimulatorRequest.ReturnDate = '2013-01-03T00:00:00+11:00';
+    service.getFlightSimulatorResponseObjects(flightSimulatorRequest)
+      .subscribe(
+        (res: FlightSimulatorResponseObject[]) => {
+          expect(res.length).toBeGreaterThan(0);
+        }
+      )
   });
 });
--- src/app/shared/modules/shared/directives/show-on-permission.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ShowOnPermissionDirective } from './show-on-permission.directive';
-
-describe('ShowOnPermissionDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ShowOnPermissionDirective();
-    expect(directive).toBeTruthy();
-  });
-});
